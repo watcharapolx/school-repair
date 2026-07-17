21178decf945f1b16591322c86fe05c9d9643f46
Watcharapol <watcharapolxd07@gmail.com>
2026-07-17 23:09:31 +0700

    feat: add DB_SSL config option to support Aiven/cloud databases

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
